d2e5dabc38f7a4034762e79d378a1aebe43652c7
Filmos <filmosyt@gmail.com>
2024-01-04 20:45:48 +0100

    Added edge labeling and node coloring in visualization

--- dtos/graph.py
+++ dtos/graph.py
@@ -1,6 +1,7 @@
 from dtos.latency_functions import LatencyFunction
 import networkx as nx
 
+COLORS = {"node": "#9293df", "flag": "#ffa45e"}
 
 class Graph:
     def __init__(self, start: str, end: str, edges: list[tuple[str, str, LatencyFunction]]):
@@ -8,11 +9,14 @@ class Graph:
         self.end = end
 
         self.G = nx.Graph()
-        self.G.add_nodes_from({e[0] for e in edges} | {e[1] for e in edges})
+        self.G.add_nodes_from(sorted({e[0] for e in edges} | {e[1] for e in edges}))
         self.G.add_edges_from([(e[0], e[1], {'cost': e[2]}) for e in edges])
 
     def draw(self) -> 'Graph':
-        nx.draw(self.G, with_labels=True)
+        # color nodes
+        pos = nx.spring_layout(self.G, seed=42)
+        nx.draw(self.G, with_labels=True, pos=pos, node_color=[COLORS['flag'] if n in [self.start, self.end] else COLORS['node'] for n in self.G.nodes])
+        nx.draw_networkx_edge_labels(self.G, pos=pos, edge_labels=nx.get_edge_attributes(self.G, 'cost'))
         return self
     
     def possible_paths(self) -> list[list[str]]:
