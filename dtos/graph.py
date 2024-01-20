25defa222df4e71f227708fe11c4f887b441c1c8
Filmos <filmosyt@gmail.com>
2024-01-20 21:49:11 +0100

    Improved flow visualization

--- dtos/graph.py
+++ dtos/graph.py
@@ -15,21 +15,23 @@ class Graph:
         self.G.add_nodes_from(sorted({e[0] for e in edges} | {e[1] for e in edges}))
         self.G.add_edges_from([(e[0], e[1], {'cost': e[2]}) for e in edges])
 
-    def draw(self, *, edge_labels: Optional[EdgeDict[str]] = None, edge_color: Optional[EdgeDict[str]] = None, width: Optional[EdgeDict[float]] = None) -> 'Graph':
+    def draw(self, *, edge_labels: Optional[EdgeDict[str]] = None, edge_color: Optional[EdgeDict[str]] = None, width: Optional[EdgeDict[float]] = None, 
+             draw_edges: bool = True, pos: Optional[dict[str, tuple[float, float]]] = None) -> 'Graph':
         def dict_to_list(d: Optional[EdgeDict[any]]) -> list[any]:
             return [d[k] for k in self.G.edges()] if d is not None else None
         
-        pos = nx.spring_layout(self.G, seed=42)
+        pos = pos or nx.spring_layout(self.G, seed=42)
         nx.draw(
             self.G, with_labels=True, pos=pos, 
             node_color=[FLAG_COLOR if n in [self.start, self.end] else NODE_COLOR for n in self.G.nodes],
-            edge_color=dict_to_list(edge_color), width=dict_to_list(width)
+            edge_color=dict_to_list(edge_color), width=(dict_to_list(width) if draw_edges else 0)
         )
 
-        labels = nx.get_edge_attributes(self.G, 'cost')
-        if edge_labels is not None:
-            labels = {k: f"{v}\n{edge_labels[k]}" for k, v in labels.items()}
-        nx.draw_networkx_edge_labels(self.G, pos=pos, edge_labels=labels)
+        if draw_edges:
+            labels = nx.get_edge_attributes(self.G, 'cost')
+            if edge_labels is not None:
+                labels = {k: f"{v}\n{edge_labels[k]}" for k, v in labels.items()}
+            nx.draw_networkx_edge_labels(self.G, pos=pos, edge_labels=labels)
 
         return self
     
