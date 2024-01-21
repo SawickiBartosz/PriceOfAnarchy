f8d831e1de76068552e2219760fb00869da62bd4
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2024-01-21 19:45:20 +0100

    add numerical example

--- solutions/__init__.py
+++ solutions/__init__.py
@@ -4,3 +4,4 @@ from .greedy import GreedySolution
 from .rotating_greedy import RotatingGreedySolution
 from .dynamic import DynamicSolution
 from .genetic import GeneticSolution
+from .numerical import NumericalSolution
