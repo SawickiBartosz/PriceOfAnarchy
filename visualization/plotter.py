a35789d7ded9a206598d536243002bac00fe4735
Szymon164 <szymonrecko1@gmail.com>
2024-01-24 19:46:34 +0100

    Examples

--- visualization/plotter.py
+++ visualization/plotter.py
@@ -6,24 +6,25 @@ from tempfile import TemporaryDirectory
 import time
 from PIL import Image
 import os
+import matplotlib.pyplot as plt
 
 class Plotter(Listener):
     def __init__(self) -> None:
         self.temp_directory = None
 
     def on_start(self, graph: Graph, core: Core) -> None:
-        if self.temp_directory is None:
-            self.temp_directory = TemporaryDirectory()
-        else:
-            print("Warning: Writer already entered")
+        self.temp_directory = TemporaryDirectory()
 
     def on_iteration(self, flow: Flow, change: Optional[Change]) -> None:
-        fig = flow.draw()
+        fig,ax = plt.subplots(1,1)
+        fig = flow.draw(ax=ax)
         fig.savefig(f'{self.temp_directory.name}/{time.time()}.png')
+        plt.close(fig)
 
     def on_end(self) -> None:
         frames=[]
-        for file in (os.listdir(self.temp_directory.name)):
+        for file in sorted(os.listdir(self.temp_directory.name)):
             frames.append(Image.open(self.temp_directory.name+"/"+file))
         frames = frames[0:1]+frames+frames[-1:]
         frames[0].save(f'outputs/{time.time()}.gif', save_all=True, append_images=frames[1:], duration=len(frames)*155, loop=0)
+        print("Gif saved in outputs")
